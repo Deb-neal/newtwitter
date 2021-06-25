4b52c8cc73ed9033531c25f0828668dffeab87ea
Deb-neal <dksaksen7@gmail.com>
2021-06-25 16:22:40 +0900

    연습용

--- Downloads/homework2/im-sprint-simple-git-workflow-master/index.js
+++ Downloads/homework2/im-sprint-simple-git-workflow-master/index.js
@@ -1,9 +1,3 @@
-let arr = [1, 4, 5, 6, 9, 10 ,15 ,16]
-let newarr = []
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {   
-        newarr.push(arr[i])
-    }
-}
-연습용 추가
+이번엔
+제발
+한번에
